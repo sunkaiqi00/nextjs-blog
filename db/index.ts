8043cd252a74b6844ddc1316e1f25dea90c321eb
kaihuai <1138245754@qq.com>
2022-07-02 16:18:48 +0800

    remotemysql

--- db/index.ts
+++ db/index.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 import { Connection, getConnection, createConnection } from 'typeorm';
 import { User, UserAuth, Article, Comment, Tag } from './entity';
 
-const type = process.env.DATEBASE_TYPE;
+const type = process.env.DATABASE_TYPE;
 const host = process.env.DATABASE_HOST;
-const port = Number(process.env.DATEBASE_PORT);
-const username = process.env.DATEBASE_USERNAME;
-const password = process.env.DATEBASE_PASSWORD;
-const database = process.env.DATEBASE_NAME;
+const port = Number(process.env.DATABASE_PORT);
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const database = process.env.DATABASE_NAME;
+
+console.log(host, port);
+
 
 let connectionRedayPromise: Promise<Connection> | null = null;
 
@@ -23,7 +26,7 @@ export const perpareConection = () => {
 
       const connection = await createConnection({
         type: 'mysql',
-        host,
+        host: 'remotemysql.com',
         port,
         username,
         password,
